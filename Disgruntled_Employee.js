ba254d2e3fc0ad64757f718ba03a0a6bdc2d199f
kay1403 <angekoumba754@gmail.com>
2025-07-31 15:38:53 +0000

    update

---
+++ Disgruntled_Employee.js
@@ -0,0 +1,11 @@
+const off =(n)=>{
+  let rst=[];
+  for (let i=0; i<n.length; i++){
+    const rst=Math.sqrt(i);
+    if(rt%1!==0){
+      rst.push(i);
+    }
+  }
+  return rst
+}
+console.log(off([5]));
