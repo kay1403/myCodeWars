356a433637b08ef6da702c0731f98101909b90ea
kay1403 <angekoumba754@gmail.com>
2025-08-06 15:12:19 +0000

    updating

--- Pair_Zeros.js
+++ Pair_Zeros.js
@@ -1,4 +1,4 @@
-//For a given list of digits 0 to 9, return a list with the same digits in the same order, but with all 0s paired. Pairing two 0s generates one 0 at the location of the first one.
+/*For a given list of digits 0 to 9, return a list with the same digits in the same order, but with all 0s paired. Pairing two 0s generates one 0 at the location of the first one.
 
 Examples
 
@@ -6,7 +6,7 @@ input: [0, 1, 0, 2]
 paired: ^-----^
     -> [0, 1,   2]
   kept: ^
-//
+*/
 const  pairZeros=(arr)=>{
   let count=0;
   return  arr.filter((val)=>{
